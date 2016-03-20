b404c502f1ce6668db0380ff646f55cbcac0772d
mikejramsey56 <mikejramsey56@gmail.com>
2016-03-20 00:18:15 -0400

    Throw illegal state exception if startTime or endTime is null

--- src/test/java/util/ElapsedTime.java
+++ src/test/java/util/ElapsedTime.java
@@ -23,14 +23,23 @@ public class ElapsedTime {
 	}
 	
 	public long getElapsedTimeNano() {
+		verifyState();
 		return Duration.between(startTime, endTime).toNanos();
 	}
 	
 	public long getElapsedTimeMilliseconds() {
+		verifyState();
 		return Duration.between(startTime, endTime).toMillis();
 	}
 	
 	public long getElapsedTimeSeconds() {
+		verifyState();
 		return Duration.between(startTime, endTime).getSeconds();
+	}
+	
+	private void verifyState() {
+		if (startTime == null || endTime == null) {
+			throw new IllegalStateException("Both start() and stop() methods must be invoked before getting elapsed time");
+		}
 	}
 }
